5ccc48570341d771bffccd943cf05dcf680dd446
vong <vongsouvan09@gmail.com>
2026-04-04 14:46:07 +0700

    feat(web): add Join Button and Shadcn Input component

--- apps/web/components/common/buttons.tsx
+++ apps/web/components/common/buttons.tsx
@@ -15,3 +15,11 @@ export function SignInButton() {
     </Button>
   );
 }
+
+export function JoinRoomButton() {
+  return (
+    <button className="w-full rounded-[4px] border border-solid border-transparent font-medium text-white px-4 py-2.5 text-sm bg-primary dark:text-black hover:translate-y-[-4px] hover:bg-transparent hover:border-primary hover:text-black hover:shadow-[0px_4px_0px_0px_var(--primary)] dark:hover:text-white transition-all duration-300">
+      Join Room
+    </button>
+  );
+}
