1870a522f7e679fe0c93e4894886a3af88492803
vong <vongsouvan09@gmail.com>
2026-04-04 15:11:30 +0700

    fix(web): add className prop to custom buttons

--- apps/web/components/common/buttons.tsx
+++ apps/web/components/common/buttons.tsx
@@ -1,24 +1,38 @@
+import { cn } from "@/lib/utils";
 import { Button } from "../ui/button";
 
-export function GetStartedButton() {
+export function GetStartedButton({ className }: { className?: string }) {
   return (
-    <button className="rounded-[4px] border border-solid border-transparent font-medium text-white px-4 py-1.5 text-sm bg-primary dark:text-black hover:translate-y-[-4px] hover:bg-transparent hover:border-primary hover:text-black hover:shadow-[0px_4px_0px_0px_var(--primary)] dark:hover:text-white transition-all duration-300">
+    <button
+      className={cn(
+        "rounded-[4px] border border-solid border-transparent font-medium text-white px-4 py-1.5 text-sm bg-primary dark:text-black hover:translate-y-[-4px] hover:bg-transparent hover:border-primary hover:text-black hover:shadow-[0px_4px_0px_0px_var(--primary)] dark:hover:text-white transition-all duration-300",
+        className,
+      )}
+    >
       Get Started
     </button>
   );
 }
 
-export function SignInButton() {
+export function SignInButton({ className }: { className?: string }) {
   return (
-    <Button variant={"ghost"} className="text-muted-foreground font-semibold">
+    <Button
+      variant={"ghost"}
+      className={cn("text-muted-foreground font-semibold", className)}
+    >
       Sign In
     </Button>
   );
 }
 
-export function JoinRoomButton() {
+export function JoinRoomButton({ className }: { className?: string }) {
   return (
-    <button className="w-full rounded-[4px] border border-solid border-transparent font-medium text-white px-4 py-2.5 text-sm bg-primary dark:text-black hover:translate-y-[-4px] hover:bg-transparent hover:border-primary hover:text-black hover:shadow-[0px_4px_0px_0px_var(--primary)] dark:hover:text-white transition-all duration-300">
+    <button
+      className={cn(
+        "w-full rounded-[4px] border border-solid border-transparent font-medium text-white px-4 py-2.5 text-sm bg-primary dark:text-black hover:translate-y-[-4px] hover:bg-transparent hover:border-primary hover:text-black hover:shadow-[0px_4px_0px_0px_var(--primary)] dark:hover:text-white transition-all duration-300",
+        className,
+      )}
+    >
       Join Room
     </button>
   );
