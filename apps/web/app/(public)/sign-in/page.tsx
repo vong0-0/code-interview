5dd0ea237746c957f80d5262533ad71cd3d1f8b8
vong <vongsouvan09@gmail.com>
2026-04-06 12:09:14 +0700

    feat(web): implement sigin in page

---
+++ apps/web/app/(public)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import SignInCard from "@/components/common/sign-in-card";
+
+export default function Page() {
+  return (
+    <section className="flex items-center justify-center flex-1">
+      <SignInCard />
+    </section>
+  );
+}
